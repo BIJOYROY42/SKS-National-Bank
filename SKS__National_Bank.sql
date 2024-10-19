c87644418f123234feaa653ae42aea93ac6b05b8
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-10-19 14:21:38 -0600

    added tables account and account types

--- SKS__National_Bank.sql
+++ SKS__National_Bank.sql
@@ -21,6 +21,10 @@ IF OBJECT_ID('Facilities', 'U') IS NOT NULL DROP TABLE Facilities;
 
 IF OBJECT_ID('Address', 'U') IS NOT NULL DROP TABLE Address;
 
+IF OBJECT_ID('Account_Types', 'U') IS NOT NULL DROP TABLE Account_Types;
+
+IF OBJECT_ID('Accounts', 'U') IS NOT NULL DROP TABLE Accounts;
+
 
 CREATE TABLE Address(
 	address_ID INT IDENTITY PRIMARY KEY,
@@ -57,4 +61,21 @@ CREATE TABLE Bank_Employees(
 	Employees_Work_AddressesWork_Address_ID INT NOT NULL,
 	FOREIGN KEY (Employees_Work_AddressesWork_Address_ID) REFERENCES Address(Address_ID) ON DELETE NO ACTION,
 	FOREIGN KEY (Employee_Address_ID) REFERENCES Address(Address_ID) ON DELETE CASCADE
+);
+
+CREATE TABLE Account_Types(
+	Account_Type_ID INT IDENTITY PRIMARY KEY,
+	Account_Type_Name VARCHAR(50) NOT NULL	
+);
+
+CREATE TABLE Accounts(
+	Account_ID INT IDENTITY PRIMARY KEY,
+	Account_Type_ID INT NOT NULL,
+	Facility_ID INT NOT NULL,
+	Balance money NOT NULL,
+	Data_Last_transaction DATE NOT NULL,
+	Check_Number INT,
+	Interest_rate DECIMAL(5,2),
+	FOREIGN KEY (Account_Type_ID) REFERENCES Account_Types(Account_Type_ID) ON DELETE CASCADE,
+	FOREIGN KEY (Facility_ID) REFERENCES Facilities(Facility_ID) ON DELETE CASCADE
 );
