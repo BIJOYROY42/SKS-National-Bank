a27d016b819f8784354e920d8f52ddd7b34fd3aa
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-11-23 18:44:11 -0700

    added accountant user

--- phase2/create_users.sql
+++ phase2/create_users.sql
@@ -5,47 +5,52 @@
 -- to customers, based on your ERD. (For example, tables related to
 -- customer information, accounts, loans, and payments).
 
--- Giving access to the customer_group_A user to the tables related to customers such as:
--- customers, accounts, customers/accounts, transfers, addresses, accounts/Employees, accounts types?
 USE Master;
-GO
+
 USE SKS_National_Bank;
+
 GO
+
 CREATE LOGIN customer_group_A WITH PASSWORD = 'customer';
+
 CREATE USER customer_group_A FOR LOGIN customer_group_A;
-GO
+
 GRANT SELECT ON Customers TO customer_group_A;
+
 GRANT SELECT ON Accounts TO customer_group_A;
+
 GRANT SELECT ON Customers_Accounts TO customer_group_A;
+
 GRANT SELECT ON Transfers TO customer_group_A;
-GRANT SELECT ON Address TO customer_group_A;  
+
+GRANT SELECT ON Address TO customer_group_A;
+
 GRANT SELECT ON Employees_Accounts TO customer_group_A;
 -- GRANT SELECT ON Account_Types TO customer_group_A;
 GO
 -- Remove the public role from the customer_group_A user
-EXEC sp_droprolemember 'public', 'customer_group_A';
+EXEC sp_droprolemember 'public',
+'customer_group_A';
+
 GO
 -- Query to check the user and their permissions
-SELECT 
+SELECT
     princ.name AS user_name,
     perm.permission_name,
     perm.state_desc AS permission_state,
     obj.name AS object_name,
     obj.type_desc AS object_type
-FROM 
-    sys.database_principals AS princ
-JOIN 
-    sys.database_permissions AS perm
-    ON perm.grantee_principal_id = princ.principal_id
-JOIN 
-    sys.objects AS obj
-    ON obj.object_id = perm.major_id
-WHERE 
+FROM sys.database_principals AS princ
+    JOIN sys.database_permissions AS perm ON perm.grantee_principal_id = princ.principal_id
+    JOIN sys.objects AS obj ON obj.object_id = perm.major_id
+WHERE
     princ.name = 'customer_group_A';
 
-select * from  Address;
-select * from Bank_Employees;
-UPDATE Address SET City_Name = 'Toronto' WHERE Address_ID = 1;
+-- select * from Address;
+
+-- select * from Bank_Employees;
+
+-- UPDATE Address SET City_Name = 'Toronto' WHERE Address_ID = 1;
 
 -- Create a login and user named “accountant_group_[?]” where [?] is your
 -- group letter. (For example, “accountant_group_B”.)
@@ -54,3 +59,34 @@ UPDATE Address SET City_Name = 'Toronto' WHERE Address_ID = 1;
 -- Their user account should not be able to update tables that are related
 -- to accounts, payments and loans, based on your ERD.
 
+CREATE LOGIN accountant_group_B WITH PASSWORD = 'accountant';
+
+CREATE USER accountant_group_B FOR LOGIN accountant_group_B;
+
+GRANT SELECT ON Accounts TO accountant_group_B;
+
+GRANT SELECT ON Transfers TO accountant_group_B;
+
+GRANT SELECT ON Account_Types TO accountant_group_B;
+
+Grant Select ON Employees_Accounts TO accountant_group_B;
+
+-- Remove the public role from the customer_group_A user
+EXEC sp_droprolemember 'public', 'accountant_group_B';
+
+GO
+-- Query to check the user and their permissions
+SELECT
+    princ.name AS user_name,
+    perm.permission_name,
+    perm.state_desc AS permission_state,
+    obj.name AS object_name,
+    obj.type_desc AS object_type
+FROM sys.database_principals AS princ
+    JOIN sys.database_permissions AS perm ON perm.grantee_principal_id = princ.principal_id
+    JOIN sys.objects AS obj ON obj.object_id = perm.major_id
+WHERE
+    princ.name = 'accountant_group_B';
+
+-- SELECT * FROM Accounts;
+-- UPDATE Accounts SET Balance = 1000 WHERE Account_ID = 1;
